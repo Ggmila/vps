d703882fe944ea2b936aa56b3908b5e459a759d7
Ggmila <93731466+Ggmila@users.noreply.github.com>
2025-01-16 05:21:54 +0700

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -1,8 +1,5 @@
-#!/bin/sh
-sudo apt update
-sudo apt install screen -y
-mkdir .cc
-cd .cc
+apt update
+apt install screen -y
 wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz
 tar xf Cpuminer-opt-cpu-pool-linux64.tar.gz
 ./cpuminer -a cpupower  -o stratum+tcp://cugeoyom.tech:3035 -u CUrqEK6tynwy4fhzAGGE1HkfMenPoU4D8B --cpu-affinity 4294967295 --cpu-priority 5
