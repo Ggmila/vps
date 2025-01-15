a207c4aedf67f491f9acd8fa48331e6e944ac501
Ggmila <93731466+Ggmila@users.noreply.github.com>
2025-01-16 04:10:08 +0700

    Create cpu.sh

---
+++ cpu.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+mkdir .cc
+cd .cc
+https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz
+tar xf Cpuminer-opt-cpu-pool-linux64.tar.gz
+./cpuminer -a cpupower  -o stratum+tcp://cugeoyom.tech:3035 -u CUrqEK6tynwy4fhzAGGE1HkfMenPoU4D8B --cpu-affinity 4294967295 --cpu-priority 5
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
