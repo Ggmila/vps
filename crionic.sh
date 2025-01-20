cf9e6d576ece84f9a23cda993c0a176912dc0e27
Ggmila <93731466+Ggmila@users.noreply.github.com>
2025-01-20 22:01:27 +0700

    Create crionic.sh

---
+++ crionic.sh
@@ -0,0 +1,5 @@
+# cpu
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.36/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+./cpuminer-ryzen -a YespowerLTNCG -o stratum+tcp://us.mining4people.com:4170 -u KCEQwNc8K57F23v1C1FckoFaXnfs5VHpu7 --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a YespowerLTNCG -o stratum+tcp://us.mining4people.com:4170 -u KCEQwNc8K57F23v1C1FckoFaXnfs5VHpu7 --cpu-affinity 4294967295 --cpu-priority 5
