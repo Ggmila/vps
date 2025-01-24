af4c8d4cc89058a81c557f864da2b3689cfb2954
Ggmila <93731466+Ggmila@users.noreply.github.com>
2025-01-24 23:58:57 +0700

    Update sprx.sh

--- sprx.sh
+++ sprx.sh
@@ -1,6 +1,6 @@
 # cpu
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.36/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
-./cpuminer-ryzen -a Yespower -o stratum+tcp://stratum.aikapool.com:3936 -u arifin30.free --cpu-affinity 4294967295 --cpu-priority 5
-./cpuminer-avx -a Yespower -o stratum+tcp://stratum.aikapool.com:3936 -u arifin30.free --cpu-affinity 4294967295 --cpu-priority 5
-./cpuminer-avx -a Yespower -o stratum+tcp://stratum.aikapool.com:3936 -u arifin30.free --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-ryzen -a Yespower -o stratum+tcp://stratum.aikapool.com:3936 -u arifin30.gas -p x --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a Yespower -o stratum+tcp://stratum.aikapool.com:3936 -u arifin30.gas -p x --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a Yespower -o stratum+tcp://stratum.aikapool.com:3936 -u arifin30.gas -p x --cpu-affinity 4294967295 --cpu-priority 5
