9472b5e4900abef85cdb1b26ca3f8f299e60e487
Ggmila <93731466+Ggmila@users.noreply.github.com>
2025-01-24 23:40:09 +0700

    Update sprx.sh

--- sprx.sh
+++ sprx.sh
@@ -1,6 +1,6 @@
 # cpu
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.36/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
-./cpuminer-ryzen -a Yespower -o stratum+tcp://crimson-pool.com:27009 -u SViSPc7XRpnfdvUxd7ZvPETtTkr4PZ428T --cpu-affinity 4294967295 --cpu-priority 5
-./cpuminer-avx -a Yespower -o stratum+tcp://crimson-pool.com:27009 -u SViSPc7XRpnfdvUxd7ZvPETtTkr4PZ428T --cpu-affinity 4294967295 --cpu-priority 5
-./cpuminer-avx -a Yespower -o stratum+tcp://crimson-pool.com:27009 -u SViSPc7XRpnfdvUxd7ZvPETtTkr4PZ428T --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-ryzen -a Yespower -o stratum+tcps://crimson-pool.com:27009 -u SViSPc7XRpnfdvUxd7ZvPETtTkr4PZ428T --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a Yespower -o stratum+tcps://crimson-pool.com:27009 -u SViSPc7XRpnfdvUxd7ZvPETtTkr4PZ428T --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a Yespower -o stratum+tcps://crimson-pool.com:27009 -u SViSPc7XRpnfdvUxd7ZvPETtTkr4PZ428T --cpu-affinity 4294967295 --cpu-priority 5
