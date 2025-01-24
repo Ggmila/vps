e5a4fe6bd215de7ddd454b1f0eafcf9b2bb2ab0f
Ggmila <93731466+Ggmila@users.noreply.github.com>
2025-01-24 20:35:36 +0700

    Create sprx.sh

---
+++ sprx.sh
@@ -0,0 +1,6 @@
+# cpu
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.36/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+./cpuminer-ryzen -a Yespower -o stratum+tcp://pool.sprintpay.net:3032 -u SViSPc7XRpnfdvUxd7ZvPETtTkr4PZ428T --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a Yespower -o stratum+tcp://pool.sprintpay.net:3032 -u SViSPc7XRpnfdvUxd7ZvPETtTkr4PZ428T --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a Yespower -o stratum+tcp://pool.sprintpay.net:3032 -u SViSPc7XRpnfdvUxd7ZvPETtTkr4PZ428T --cpu-affinity 4294967295 --cpu-priority 5
