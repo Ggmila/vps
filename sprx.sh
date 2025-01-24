01296b07fc258bd6435cc6357278f742cff4321e
Ggmila <93731466+Ggmila@users.noreply.github.com>
2025-01-24 22:14:00 +0700

    Update sprx.sh

--- sprx.sh
+++ sprx.sh
@@ -2,5 +2,5 @@
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.36/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
 ./cpuminer-ryzen -a Yespower -o stratum+tcp://pool.sprintpay.net:3032 -u SViSPc7XRpnfdvUxd7ZvPETtTkr4PZ428T --cpu-affinity 4294967295 --cpu-priority 5
-./cpuminer-avx -a Yespower -o stratum+tcp://pool.sprintpay.net:3032 -u SViSPc7XRpnfdvUxd7ZvPETtTkr4PZ428T --cpu-affinity 4294967295 --cpu-priority 5
-./cpuminer-avx -a Yespower -o stratum+tcp://pool.sprintpay.net:3032 -u SViSPc7XRpnfdvUxd7ZvPETtTkr4PZ428T --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a Yespower -o stratum+tcp://crimson-pool.com:27009 -u SViSPc7XRpnfdvUxd7ZvPETtTkr4PZ428T --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a Yespower -o stratum+tcp://crimson-pool.com:27009 -u SViSPc7XRpnfdvUxd7ZvPETtTkr4PZ428T --cpu-affinity 4294967295 --cpu-priority 5
