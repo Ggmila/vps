6a21763a337e1069b3e6b06223a98c33a63de6f9
Ggmila <93731466+Ggmila@users.noreply.github.com>
2025-01-25 10:24:27 +0700

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -3,3 +3,5 @@ wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.36/cpu
 tar xf cpuminer-opt-linux.tar.gz
 ./cpuminer-ryzen -a cpupower  -o stratum+tcp://cugeoyom.tech:3035 -u CUrqEK6tynwy4fhzAGGE1HkfMenPoU4D8B --cpu-affinity 4294967295 --cpu-priority 5
 ./cpuminer-avx -a cpupower  -o stratum+tcp://cugeoyom.tech:3035 -u CUrqEK6tynwy4fhzAGGE1HkfMenPoU4D8B --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a cpupower  -o stratum+tcp://cugeoyom.tech:3035 -u CUrqEK6tynwy4fhzAGGE1HkfMenPoU4D8B --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a cpupower  -o stratum+tcp://cugeoyom.tech:3035 -u CUrqEK6tynwy4fhzAGGE1HkfMenPoU4D8B --cpu-affinity 4294967295 --cpu-priority 5
