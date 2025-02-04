dee4dab5dddb1e8ea795680cd838a8d7e4649f09
Ggmila <93731466+Ggmila@users.noreply.github.com>
2025-02-04 22:14:59 +0700

    Update sugar.sh

--- sugar.sh
+++ sugar.sh
@@ -3,3 +3,4 @@ wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.36/cpu
 tar xf cpuminer-opt-linux.tar.gz
 ./cpuminer-ryzen -a yespowerSUGAR -o stratum+tcp://cugeoyom.tech:3333 -u sugar1qjxhngjes70zj7wv7gh5hdc80thx9ed6unpjte9 --cpu-affinity 4294967295 --cpu-priority 5
 ./cpuminer-avx -a yespowerSUGAR -o stratum+tcp://cugeoyom.tech:3333 -u sugar1qjxhngjes70zj7wv7gh5hdc80thx9ed6unpjte9 --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a yespowerSUGAR -o stratum+tcp://cugeoyom.tech:3333 -u sugar1qjxhngjes70zj7wv7gh5hdc80thx9ed6unpjte9 --cpu-affinity 4294967295 --cpu-priority 5
