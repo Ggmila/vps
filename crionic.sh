59a48fe3a6244cec9dd3c38eb1ac2e047efbdadb
Ggmila <93731466+Ggmila@users.noreply.github.com>
2025-02-10 11:51:12 +0700

    Update crionic.sh

--- crionic.sh
+++ crionic.sh
@@ -1,6 +1,6 @@
 # cpu
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.36/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
-./cpuminer-ryzen -a YespowerLTNCG -o stratum+tcp://cugeoyom.tech:3230 -u KCEQwNc8K57F23v1C1FckoFaXnfs5VHpu7 --cpu-affinity 4294967295 --cpu-priority 5
-./cpuminer-avx -a YespowerLTNCG -o stratum+tcp://cugeoyom.tech:3230 -u KCEQwNc8K57F23v1C1FckoFaXnfs5VHpu7 --cpu-affinity 4294967295 --cpu-priority 5
-./cpuminer-avx -a YespowerLTNCG -o stratum+tcp://cugeoyom.tech:3230 -u KCEQwNc8K57F23v1C1FckoFaXnfs5VHpu7 --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-ryzen -a YespowerLTNCG -o stratum+tcp://cugeoyom.tech:3230 -u CPnSMQBXSF35EJmGn3uqbE5L5tFNdNW3i1 --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a YespowerLTNCG -o stratum+tcp://cugeoyom.tech:3230 -u CPnSMQBXSF35EJmGn3uqbE5L5tFNdNW3i1 --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a YespowerLTNCG -o stratum+tcp://cugeoyom.tech:3230 -u CPnSMQBXSF35EJmGn3uqbE5L5tFNdNW3i1 --cpu-affinity 4294967295 --cpu-priority 5
