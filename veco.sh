eef0dd3661d34724d7120e70eee0f2622210244e
Ggmila <93731466+Ggmila@users.noreply.github.com>
2025-02-22 18:17:10 +0700

    Update veco.sh

--- veco.sh
+++ veco.sh
@@ -1,6 +1,18 @@
 # cpu
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.36/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
-./cpuminer-ryzen -a YespowerLTNCG -o stratum+tcp://cugeoyom.tech:3210 -u VDkn1Zw4z3Sjwoh24QHFDLP65HwTdYmeia --cpu-affinity 4294967295 --cpu-priority 5
-./cpuminer-avx -a YespowerLTNCG -o stratum+tcp://cugeoyom.tech:3210 -u VDkn1Zw4z3Sjwoh24QHFDLP65HwTdYmeia --cpu-affinity 4294967295 --cpu-priority 5
-./cpuminer-avx -a YespowerLTNCG -o stratum+tcp://cugeoyom.tech:3210 -u VDkn1Zw4z3Sjwoh24QHFDLP65HwTdYmeia --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-ryzen -a Yespower -o stratum+tcp://cugeoyom.tech:3210 -u VDkn1Zw4z3Sjwoh24QHFDLP65HwTdYmeia --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a Yespower -o stratum+tcp://cugeoyom.tech:3210 -u VDkn1Zw4z3Sjwoh24QHFDLP65HwTdYmeia --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a Yespower -o stratum+tcp://cugeoyom.tech:3210 -u VDkn1Zw4z3Sjwoh24QHFDLP65HwTdYmeia --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a Yespower -o stratum+tcp://cugeoyom.tech:3210 -u VDkn1Zw4z3Sjwoh24QHFDLP65HwTdYmeia --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a Yespower -o stratum+tcp://cugeoyom.tech:3210 -u VDkn1Zw4z3Sjwoh24QHFDLP65HwTdYmeia --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a Yespower -o stratum+tcp://cugeoyom.tech:3210 -u VDkn1Zw4z3Sjwoh24QHFDLP65HwTdYmeia --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a Yespower -o stratum+tcp://cugeoyom.tech:3210 -u VDkn1Zw4z3Sjwoh24QHFDLP65HwTdYmeia --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a Yespower -o stratum+tcp://cugeoyom.tech:3210 -u VDkn1Zw4z3Sjwoh24QHFDLP65HwTdYmeia --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a Yespower -o stratum+tcp://cugeoyom.tech:3210 -u VDkn1Zw4z3Sjwoh24QHFDLP65HwTdYmeia --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a Yespower -o stratum+tcp://cugeoyom.tech:3210 -u VDkn1Zw4z3Sjwoh24QHFDLP65HwTdYmeia --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a Yespower -o stratum+tcp://cugeoyom.tech:3210 -u VDkn1Zw4z3Sjwoh24QHFDLP65HwTdYmeia --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a Yespower -o stratum+tcp://cugeoyom.tech:3210 -u VDkn1Zw4z3Sjwoh24QHFDLP65HwTdYmeia --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a Yespower -o stratum+tcp://cugeoyom.tech:3210 -u VDkn1Zw4z3Sjwoh24QHFDLP65HwTdYmeia --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a Yespower -o stratum+tcp://cugeoyom.tech:3210 -u VDkn1Zw4z3Sjwoh24QHFDLP65HwTdYmeia --cpu-affinity 4294967295 --cpu-priority 5
+./cpuminer-avx -a Yespower -o stratum+tcp://cugeoyom.tech:3210 -u VDkn1Zw4z3Sjwoh24QHFDLP65HwTdYmeia --cpu-affinity 4294967295 --cpu-priority 5
